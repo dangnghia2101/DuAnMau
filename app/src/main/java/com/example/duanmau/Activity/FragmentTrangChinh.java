e066b5c904412cf15e57b68ca833c2dba6cd5db6
dangnghia21 <nghiadtps16665@fpt.edu.vn>
2021-09-26 21:34:12 +0700

    update

---
+++ app/src/main/java/com/example/duanmau/Activity/FragmentTrangChinh.java
@@ -0,0 +1,32 @@
+package com.example.duanmau.Activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duanmau.R;
+
+public class FragmentTrangChinh extends Fragment {
+
+    public FragmentTrangChinh() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trang_chinh, container, false);
+    }
+}
--- app/src/main/java/com/example/duanmau/Activity/ThanhvienActivity.java
+++ app/src/main/java/com/example/duanmau/Activity/ThanhvienActivity.java
@@ -1,89 +1,59 @@
-package com.example.duanmau;
+package com.example.duanmau.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
-import android.view.Gravity;
-import android.view.MenuItem;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.example.duanmau.Activity.FragmentThanhVien;
-import com.example.duanmau.Activity.FragmentTrangChinh;
-import com.example.duanmau.Activity.ThanhvienActivity;
+import com.example.duanmau.MainActivity;
+import com.example.duanmau.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class ThanhvienActivity extends AppCompatActivity {
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
     public NavigationView navigationView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_thanhvien);
 
-        openNavigation();
-
-
+        OpenNavigasionView();
     }
 
-    // override the onOptionsItemSelected()
-    // function to implement
-    // the item click listener callback
-    // to open and close the navigation
-    // drawer when the icon is clicked
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void openNavigation(){
-        //Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
+    public void OpenNavigasionView(){
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
         drawerLayout = findViewById(R.id.my_drawer_layout);
         navigationView = findViewById(R.id.nav_menu);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.nav_open, R.string.nav_close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
 
         // pass the Open and Close toggle for the drawer layout listener
         // to toggle the button
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.nav_TrangChinh_fragment, new FragmentTrangChinh()).commit();
-        navigationView.setCheckedItem(R.id.nav_PhieuMuon);
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Event CLick navigation
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            Fragment temp;
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 switch (item.getItemId()){
                     case R.id.nav_ThanhVien:
-                        temp = new FragmentThanhVien();
+                        Toast.makeText(ThanhvienActivity.this, "Click thành viên", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ThanhvienActivity.this, ThanhvienActivity.class));
                         break;
                     case R.id.nav_PhieuMuon:
-                        temp = new FragmentTrangChinh();
                         break;
                     case R.id.nav_LoaiSach:
                         break;
@@ -99,12 +69,23 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_TrangChinh_fragment, temp).commit();
-
-                drawerLayout.closeDrawer(GravityCompat.START);
 
                 return false;
             }
         });
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
